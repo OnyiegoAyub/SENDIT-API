fe74ce9420220b54906ac1fc3457e7556aee9d1d
Onyiego Ayub <onyiegoayub@gmail.com>
2018-11-15 22:39:57 +0300

    [Chore #161986255] update tests

--- app/tests/v1/test_data_parcel.py
+++ app/tests/v1/test_data_parcel.py
@@ -36,13 +36,7 @@ class TestParcelCase(unittest.TestCase):
     response = self.app.get('api/v1/parcels/1')
     self.assertEqual(response.status_code, 200)
 
-  def test_create_user(self):
-    response = self.app.post('/api/v1/users', data=json.dumps(self.data), content_type='application/json')
-    self.assertEqual(response.status_code, 201)
-
-  def test_get_all_users(self):
-    response = self.app.get('api/v1/users')
-    self.assertEqual(response.status_code, 200)
+  
 
 if __name__ == '__main__':
   unittest.main()
