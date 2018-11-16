78ee37928ac2ebd0c8328e7dd1c7f866c304f984
OnyiegoAyub <38955236+OnyiegoAyub@users.noreply.github.com>
2018-11-16 11:42:47 +0300

    Update test_user_auth.py

--- app/tests/v1/test_user_auth.py
+++ app/tests/v1/test_user_auth.py
@@ -8,9 +8,9 @@ class TestUserCase(unittest.TestCase):
 
   def setUp(self):
 
-    create_app().testing = True #???
+    create_app().testing = True
     '''create an instance of a flask application just as you would create  a flask app, but in this case you pass to it a test client which means you are running a test instance'''
-    self.app = create_app().test_client() #???
+    self.app = create_app().test_client() 
     self.data = {
         "user_id": 1,
         "username": "Ayub",
@@ -30,19 +30,8 @@ class TestUserCase(unittest.TestCase):
       self.assertEqual(response.status_code, 200)
 
 
-#   def test_create_user(self):
-#     response = self.app.post('/api/v1/users', data=json.dumps(self.data), content_type='application/json')
-#     self.assertEqual(response.status_code, 201)
-#
-#   def test_get_all_users(self):
-#     response = self.app.get('api/v1/users')
-#     self.assertEqual(response.status_code, 200)
-#
-#   def test_get_single_user(self):
-#     response = self.app.get('api/v1/users/1')
-#     self.assertEqual(response.status_code, 200)
-#
-#
-#
-# if __name__ == '__main__':
-#   unittest.main()
+
+
+
+ if __name__ == '__main__':
+   unittest.main()
